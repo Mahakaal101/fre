b36e07774289db62fb795fa2afe19fd8ee199364
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-03-11 12:36:27 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -9,14 +9,15 @@ load_dotenv()
 
 
 class Var(object):
-    MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    MULTI_CLIENT = True
+    API_ID = int(environ.get("API_ID", 15823382))
+    API_HASH = str(environ.get("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c"))
+    MULTI_TOKEN1=:5499850853:AAF1saxsT9Q46H7zGs90asuTpAqND5qDgnI
+    MULTI_TOKEN2=6213184839:AAGDu5TrR6ZiUPB9n82e6z_Anb_b6gMr_mA
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
+        environ.get("BIN_CHANNEL", -100175430918)
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
@@ -26,7 +27,7 @@ class Var(object):
     HASH_LENGTH = int(environ.get("HASH_LENGTH", 6))
     if not 5 < HASH_LENGTH < 64:
         sys.exit("Hash length should be greater than 5 and less than 64")
-    FQDN = str(environ.get("FQDN", BIND_ADDRESS))
+    FQDN = str(environ.get("FQDN", "http://143.110.250.241"))
     URL = "http{}://{}{}/".format(
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
